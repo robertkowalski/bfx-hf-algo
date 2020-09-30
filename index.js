f9ad358eed071d4b332682492716a0b59afad1ce
Robert Kowalski <rok@kowalski.gd>
2020-09-30 17:16:37 +0200

    wip

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ module.exports = {
   PingPong: require('./lib/ping_pong'),
   MACrossover: require('./lib/ma_crossover'),
   OCOCO: require('./lib/ococo'),
+  HedgedOrder: require('./lib/hedged_order'),
   AsyncEventEmitter: require('./lib/async_event_emitter')
 }
